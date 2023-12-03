6e9458d32bbb14908d571ed18141772cdd14a714
mbovenizer <mbovenizer@outlook.ie>
2023-12-03 22:44:14 +0000

    added a failed test , commented out

--- src/test/java/org/example/ColourTableTest.java
+++ src/test/java/org/example/ColourTableTest.java
@@ -60,7 +60,19 @@ public class ColourTableTest {
         assertFalse(ColourTable.areRGBValuesValid(2600, 3450, 7660));
     }
 
-
+    //    this test fails : this was the original test I made and used when writing up my report as I did think it was wrong
+//    as it was failing , but it was failing correctly with the message being outputted as there were duplicates being repeatedly added
+//    @Test
+//    @DisplayName("Test exceeding the capacity of ColourTable")
+//    void testExceedCapacity() {
+//        // Add colors up to the capacity
+//        for (int i = 0; i < 8; i++) {
+//            colourTable.add(70, 90, 100);
+//        }
+//
+//        // Attempt to add another color with invalid RGB values, expecting an exception
+//        assertThrows(IllegalArgumentException.class, () -> colourTable.add(256, 256, 257));
+//    }
     //    changed what I was adding to show the test passes when unique elements are added
     @Test
     @DisplayName("Test exceeding the capacity of ColourTable")
